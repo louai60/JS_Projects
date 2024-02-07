642a85849e266fa56fa589c89eff98e921bfa5c8
louai <louaysaafi60@gmail.com>
2024-02-07 14:59:06 +0100

    button_clicker

--- Button_Clicker/script.js
+++ Button_Clicker/script.js
@@ -1,7 +1,14 @@
-function logout(element) {
-    element.innerText = "LogOut";
-}
+let btn = document.querySelector('.nav-btn');
 
+function change() {
+    if(btn.innerText=="Login"){
+        btn.innerText="Logout";
+    } else {
+        btn.innerText="Login";
+    }
+ 
+}
 function hide(element) {
     element.remove();
 }
+
