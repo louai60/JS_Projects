53082f8a871fea3d15516d869ec41815fe4ac63d
louai <louaysaafi60@gmail.com>
2024-02-07 14:02:18 +0100

    Making Connections

--- video-preview/script.js
+++ video-preview/script.js
@@ -1,19 +1,12 @@
 console.log("page loaded...");
 
-function over(element) {
-    element.src="assets/Aston_Martin_hover.png"   
-}
-    
-function out(element) {
-    element.src="assets/Aston_Martin.png"      
-}
 
 function playVideo(element) {
-    element.muted = true;
-    element.play();
-  }
-
-  function pauseVideo(element) {
     element.muted = false;
-    element.pause()
-  }
+    element.play();
+}
+
+function pauseVideo(element) {
+  element.muted = true;
+  element.pause()
+}
